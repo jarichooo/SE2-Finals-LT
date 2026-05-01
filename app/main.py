5f74e42b12aee94da5f40ae42ac24f198952e88f
Joshua Jericho Barja <jarichooo@192.168.0.125>
2026-05-01 14:03:25 +0800

    Update main.py

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello, World!"
+
+
+@app.route('/add/<int:a>/<int:b>')
+def add(a, b):
+    return str(a + b)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
